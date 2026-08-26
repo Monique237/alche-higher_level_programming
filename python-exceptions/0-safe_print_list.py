11c848c548ebf17b8dd0de7843b3efa5bd114c44
Monique237 <m.nchiefieh@alustudent.com>
2026-08-26 06:38:50 -0700

    Add safe list printing task

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     count = 0
-    while count < x:
-        try:
-            print(my_list[count], end="")
+
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
             count += 1
-        except IndexError:
-            break
+    except IndexError:
+        pass
+
     print()
     return count
