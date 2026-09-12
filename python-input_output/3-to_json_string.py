7fcaf95ca992b1f8954ba6db590c987643004695
Monique237 <m.nchiefieh@alustudent.com>
2026-09-12 12:54:34 -0700

    Add JSON serialization function

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Provide a function that converts a Python object to a JSON string."""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON string representation of a Python object."""
+    return json.dumps(my_obj)
